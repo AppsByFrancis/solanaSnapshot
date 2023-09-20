3ae9e45da54ab629f6994bc4f30d109f86f3f436
AppsByFrancis <majer.frantisek@hotmail.com>
2023-09-20 13:34:16 +0200

    IndiLab-01: Collection search working

--- src/components/findFunction.js
+++ src/components/findFunction.js
@@ -3,23 +3,6 @@
 const findCollection = async (address) => {
     const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-    // const rateLimitedAsyncFunc = async (owner, amount) => {
-    //     while (true) { // keep trying until successful or non 429 error
-    //         try {
-    //             await asyncFunc(owner, amount);
-    //             break; // exit loop if successful
-    //         } catch (err) {
-    //             if (err.message.includes('429')) { // check for 429 status in the error
-    //                 console.log("Rate limited! Waiting before retry...");
-    //                 await sleep(1000 * 10); // wait for 10 seconds before retrying
-    //             } else {
-    //                 throw err; // if error is not 429, throw it
-    //             }
-    //         }
-    //     }
-    // };
-
-
     var myHeaders = new Headers();
     myHeaders.append("x-api-key", "DusIOcbkZAl81jAt");
 
@@ -32,7 +15,7 @@ const findCollection = async (address) => {
 
 
     try {
-        const rawCollection = await fetch(`https://api.shyft.to/sol/v1/collections/get_nfts?network=mainnet-beta&collection_address=${address}&page=26&size=15`, requestOptions)
+        const rawCollection = await fetch(`https://api.shyft.to/sol/v1/collections/get_nfts?network=mainnet-beta&collection_address=${address}&page=1&size=60`, requestOptions)
         const jsonCollection =  await rawCollection.json();
         const nftsOfCollection = await jsonCollection.result?.nfts;
         const totalPages = await jsonCollection.result?.total_pages;
@@ -52,19 +35,18 @@ const findCollection = async (address) => {
                 await sleep(1000)
                  
                  console.log("started")
-                const rawPageResponse = await fetch(`https://api.shyft.to/sol/v1/collections/get_nfts?network=mainnet-beta&collection_address=${address}&page=${page}&size=30`, requestOptions);
+                const rawPageResponse = await fetch(`https://api.shyft.to/sol/v1/collections/get_nfts?network=mainnet-beta&collection_address=${address}&page=${page}&size=60`, requestOptions);
                 const jsonPageData = await rawPageResponse?.json();
                 const nfts = await jsonPageData.result?.nfts;
                 allNFTs = allNFTs.concat(nfts);
                 
+                console.log(`Fetch: ${page}`)
                 
             }
             
             
             console.log(allNFTs)
 
-        console.log(nftsOfCollection) 
-        console.log(totalPages)
         
     } catch (err) {
         console.error(err)
